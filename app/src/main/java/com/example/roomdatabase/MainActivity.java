8aac45c7485a94b7152a95e104ce0b0043eb3e38
Anandkonda765 <anand@vedaslabs.com>
2022-11-26 22:38:36 +0530

    FloatingActionButton Added

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -20,7 +20,7 @@ import com.example.roomdatabase.Model.StudentFamilyDetails;
 public class MainActivity extends AppCompatActivity {
     DataBase dataBase;
     EditText name, fatherName, motherName, phoneNumber;
-    Button submit;
+    Button submit,viewUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         motherName = findViewById(R.id.mother_Name);
         phoneNumber = findViewById(R.id.phone_number);
         submit = findViewById(R.id.submit);
-        //viewUsers=findViewById(R.id.viewusers);
+        viewUsers=findViewById(R.id.viewusers);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,6 +72,13 @@ public class MainActivity extends AppCompatActivity {
 
 
                 }
+            }
+        });
+        viewUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+
             }
         });
 
